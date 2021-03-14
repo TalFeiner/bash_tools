27f0ac162a4445319e68fc4f547d139f5892d159
Tal Feiner <feinertal@gmail.com>
2021-03-14 13:58:16 +0200

    remove tf2_ros for py3

--- ros-workspace_py2-alongside-py3.sh
+++ ros-workspace_py2-alongside-py3.sh
@@ -46,10 +46,10 @@ if [ ! -z "$dir_ws" ]; then
         catkin_make -DPYTHON_EXECUTABLE=/usr/bin/python3
         if [ $? -eq 0 ]; then
             source devel/setup.bash
-            wstool init
-            wstool set -y src/geometry2 --git https://github.com/ros/geometry2 -v 0.6.5
-            wstool up
-            rosdep install --from-paths src --ignore-src -y -r
+            #  wstool init
+            #  wstool set -y src/geometry2 --git https://github.com/ros/geometry2 -v 0.6.5
+            #  wstool up
+            #  rosdep install --from-paths src --ignore-src -y -r
             cp -TR $HOME/tmp $HOME/$workspace/src
             catkin_make --cmake-args -DCMAKE_BUILD_TYPE=Release -DPYTHON_EXECUTABLE=/usr/bin/python3 -DPYTHON_INCLUDE_DIR=/usr/include/python3.6m -DPYTHON_LIBRARY=/usr/lib/x86_64-linux-gnu/libpython3.6m.so
             if [ $? -eq 0 ]; then
